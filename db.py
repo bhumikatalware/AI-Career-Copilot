9086ca25a43852d900cf3248610858fece0b21ea
Bhumika pradeep Talware <bhumikatalware70@gmail.com>
2026-07-16 13:31:26 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = (
-    "mysql+pymysql://26r2cALXrd5vqJS.root:"
-    "hkEu4u9t7UlZLbC1@"
-    "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/test"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
